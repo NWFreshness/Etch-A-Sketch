458f9d6a8e1afdf1066619cf39ef3e4de6f23d55
Tyler Mayfield <tyler.mayfield23@gmail.com>
2022-03-10 16:09:45 -0800

    Adding the hover & clear functionality

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 const container = document.querySelector('.container');
+const clear = document.querySelector('.clear');
 
-function createGrid() {
-    for(let i = 0; i < 256; i++){
+function createGrid(num) {
+    let size = num * num;
+    for(let i = 0; i < size; i++){
         const div = document.createElement('div')
+        div.classList.add('box');
         container.append(div);
     }
+
+    const gridBoxes = document.querySelectorAll('.box');
+
+    gridBoxes.forEach((box)=>{
+        box.addEventListener('mouseenter', ()=>{
+            box.classList.add('active');
+        });
+    })
 }
-createGrid();
+createGrid(16);
+
+
+
+clear.addEventListener('click', ()=>{
+    // gridBoxes.forEach((box)=>{
+    //     box.classList.remove('active');
+    // });
+    container.querySelectorAll('*').forEach(e => e.remove());
+    // while(container.firstChild){
+    //     container.remove(container.firstChild);
+    // }
+
+    const userInput = +prompt('What grid dimensions would you like?');
+    createGrid(userInput)
+    container.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${userInput}, 1fr)`
+    
+})
+
+
+
+
